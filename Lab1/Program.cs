dfc675219c1464e6e444f6fe43aed2ede846b608
ericwebdev00 <111818435+ericwebdev00@users.noreply.github.com>
2022-09-04 21:42:19 -0400

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+                Console.WriteLine("Hello World!");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Please enter your age ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("You are an Adult");
+            }
+            else
+            {
+                Console.WriteLine("You are a Child");
+            }
+            Console.ReadLine();
+        }
+    }
+}
